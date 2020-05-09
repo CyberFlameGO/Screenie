1c3d45afcba1b95b07b732d1991831b03e24f0d3
gp2-marr <george.marr.cobc@gmail.com>
2020-05-09 23:42:10 +0100

    fixes the check ffmpeg

--- main/main.go
+++ main/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	//first need to check whether or not ffmpeg is going to be available
 	//this is required for the video output to be working
-	cmd := exec.Command("ffmpeg", "-v")
+	cmd := exec.Command("ffmpeg", "-version")
 	if err := cmd.Run(); err != nil {
 		fmt.Print("ffmpeg is not installed or could not be found in the OS path, exiting...")
 		os.Exit(1)
