3ccba4578dd0c936a5e26cbf78fd19da712e2960
gp2-marr <george.marr.cobc@gmail.com>
2020-05-10 21:32:23 +0100

    gonna make the interface a bit better

--- ui/interface.go
+++ ui/interface.go
@@ -1,12 +1,7 @@
 package ui
 
 import (
-	"fmt"
 	fyne "fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
-	"log"
-	"screenie/capture"
-	"time"
 )
 
 var (
@@ -14,25 +9,5 @@ var (
 )
 
 func InitInterface() {
-	w := app.NewWindow("Screenie")
-	//w.SetFixedSize(true)
 
-	w.SetContent(widget.NewVBox(widget.NewLabel("Init"),
-		widget.NewButton("Quit", func() {
-			app.Quit()
-		}),
-		widget.NewButton("Record", func() {
-			capture.VideoRecordingRunner()
-		}),
-		widget.NewButton("Screenshot", func() {
-			err := capture.SaveScreenshot(fmt.Sprintf("MM-DD-YYYY", time.Now())) //todo ask for them to put in the name
-			if err != nil {
-				log.Fatalf("could not start new recording at: %s with error: %v", time.Now(), err.Error())
-			}
-		}),
-		widget.NewButton("Stop recording", func() {
-			capture.StopRecording()
-		})))
-
-	w.ShowAndRun()
 }
