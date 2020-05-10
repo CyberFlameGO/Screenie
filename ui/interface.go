85de2d36363e584f91c419ed996a7b1326746c9a
gp2-marr <george.marr.cobc@gmail.com>
2020-05-10 21:39:15 +0100

    it looks horrible but sod it

--- ui/interface.go
+++ ui/interface.go
@@ -1,13 +1,45 @@
 package ui
 
 import (
-	fyne "fyne.io/fyne/app"
+	"fmt"
+	"fyne.io/fyne"
+	f "fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/theme"
+	Screenie "screenie"
+	"screenie/capture"
+	"time"
 )
 
 var (
-	app = fyne.New()
+	app = f.New()
 )
 
 func InitInterface() {
+	app.Settings().SetTheme(theme.DarkTheme()) //todo get the theme from the config
+	w := app.NewWindow("Screenie")
+	w.SetContent(fyne.NewContainerWithLayout(
+		layout.NewFormLayout(),
+		Screenie.FyneGroup("primary_interface", true,
+			fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+				Screenie.FyneButton("Quit", func() {
+					app.Quit()
+				}),
+				fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+					Screenie.FyneButton("Start Recording", func() {
+						capture.VideoRecordingRunner()
+					}),
+					fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+						Screenie.FyneButton("Stop Recording", func() {
+							capture.StopRecording()
+						}),
+						fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+							Screenie.FyneButton("Screenshot", func() {
+								capture.SaveScreenshot(fmt.Sprintf("DD-MM-YYYY", time.Now()))
+							}),
+							fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+								Screenie.FyneButton("Settings", func() {
+									//todo
+								})))))))))
 
 }
