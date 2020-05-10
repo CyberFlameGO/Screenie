740ca435330558c3e8effda11a6f129144ba96f7
gp2-marr <george.marr.cobc@gmail.com>
2020-05-10 20:58:18 +0100

    this should make things a little better to make

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package Screenie
 
 import (
+	"fyne.io/fyne/widget"
 	"github.com/lxn/win"
 )
 
@@ -12,4 +13,20 @@ func GetScreenHeight() int {
 
 func GetScreenWidth() int {
 	return int(win.GetDeviceCaps(hDC, win.VERTRES))
+}
+
+func FyneLabel(text string) *widget.Label {
+	w := widget.NewLabel(text)
+	return w
+}
+
+func FyneEntry(placeHolder string) *widget.Entry {
+	w := widget.NewEntry()
+	w.SetPlaceHolder(placeHolder)
+	return w
+}
+
+func FyneButton(text string, action func()) *widget.Button {
+	w := widget.NewButton(text, action)
+	return w
 }
